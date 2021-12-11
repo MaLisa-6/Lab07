f5fdf8eec9ae2b4d2ed1db32304527319b47a35c
Dan He <dhe36@wisc.edu>
2021-12-10 22:13:21 -0600

    calculate maximum value for each city

---
+++ mapper_max.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+
+def mapper():
+    for line in sys.stdin:
+        if len(line.strip().split(",")) == 4:
+            data = line.strip().split(",")
+            timestamp, city, item, cost = data
+            print(city + "," + cost)
+        elif len(line.strip().split(",")) > 4:
+            data = line.strip().split(",")[0:4]
+            timestamp, city, item, cost = data
+            print(city + "," + cost)
+        else:
+            pass
+            
+if __name__ == "__main__":
+    # what function should run when python mapper.py is called?
+    mapper()
