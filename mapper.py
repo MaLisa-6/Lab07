aa32f990e6731c6bdf18bdf2911f67335bde1e6b
Dan He <dhe36@wisc.edu>
2021-12-10 22:02:03 -0600

    solve bad input error

--- mapper.py
+++ mapper.py
@@ -4,9 +4,16 @@ import sys
 
 def mapper():
     for line in sys.stdin:
-        data = line.strip().split(",")
-        timestamp, city, item, cost = data
-        print(city + "," + cost)
+        if len(line.strip().split(",")) == 4:
+            data = line.strip().split(",")
+            timestamp, city, item, cost = data
+            print(city + "," + cost)
+        elif len(line.strip().split(",")) > 4:
+            data = line.strip().split(",")[0:4]
+            timestamp, city, item, cost = data
+            print(city + "," + cost)
+        else:
+            pass
 
 if __name__ == "__main__":
     # what function should run when python mapper.py is called?
